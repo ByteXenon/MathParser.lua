65bc0d4ad276fb8ddc28e85beab34c596e7690f3
Luna Davis <ddavi142@asu.edu>
2024-01-11 15:16:29 -0700

    Add more tests, and add tests for the new features

--- tests/init.lua
+++ tests/init.lua
@@ -11,25 +11,31 @@ package.path = requirePath
 -- Dependencies
 local MathParser = require("MathParser")
 
--- Create an instance of MathParser with custom variables
-local myMathParser = MathParser:new(nil, VARIABLES)
+-- Create an instance of MathParser
+local myMathParser = MathParser:new()
 
--- Add some variables
-myMathParser:setVariable("test", 10)
-myMathParser:setVariable("a", 5)
-
--- Unit tests
 local function runUnitTest(expression, expected, testName)
   local result = myMathParser:solve(expression)
   assert(result == expected, testName .. " failed. Expected: " .. expected .. ", Actual: " .. result)
   print("Successfully passed unit test: '" .. testName .. "' With result: " .. result)
 end
 
+-- Add some variables
+myMathParser:addVariable("test", 10)
+myMathParser:addVariable("a", 5)
+
+-- Add some functions
+myMathParser:addFunction("testFunction", function(a, b)
+  return a + b
+end)
+
+-- Unit tests
 runUnitTest("2 + 3", 5, "Addition")
 runUnitTest("5 - 2", 3, "Subtraction")
 runUnitTest("2 * 3", 6, "Multiplication")
 runUnitTest("6 / 2", 3, "Division")
 runUnitTest("2 ^ 3", 8, "Exponentiation")
+runUnitTest("2 + 3 * 4", 14, "Operator precedence (multiplication)")
 runUnitTest("2 ^ 3 ^ 2", 512, "Exponentiation (right-associative)")
 runUnitTest("a + test", 15, "Variable substitution")
 runUnitTest("(2 + 3) * 4 - 5", 15, "Complex expression")
@@ -38,14 +44,21 @@ runUnitTest(".5 + .5", 1, "Floating point numbers (leading decimal point)")
 runUnitTest("2e3 + 2e+3", 4000, "Scientific notation (positive exponent)")
 runUnitTest("2e3 - 2e-3", 1999.998, "Scientific notation (negative exponent)")
 runUnitTest(".5e3 + .5e-3", 500.0005, "Scientific notation (leading decimal point)")
-
+runUnitTest("1.10e+5", 110000, "Scientific notation (leading decimal point and positive exponent)")
+runUnitTest("0xFF + 0xFF", 510, "Hexadecimal numbers")
+runUnitTest("sin(1)", math.sin(1), "Function call (sin)")
+runUnitTest("-sin(1)", -math.sin(1), "Function call (sin) with unary operator")
+runUnitTest("sin(sin(1))", math.sin(math.sin(1)), "Function call (sin) with nested function call")
+runUnitTest("log(10, 100)", math.log(10, 100), "Function call with multiple arguments (log)")
+runUnitTest("log(sin(1), cos(1))", math.log(math.sin(1), math.cos(1)), "Function call (log) with multiple arguments-function-calls (sin, cos)")
+runUnitTest("testFunction(2, 3)", 5, "Custom function call (testFunction)")
 
 -- Error handling tests
 local function runErrorHandlingTest(expression, testName)
   local status, err = pcall(function()
     myMathParser:solve(expression)
   end)
-  assert(not status, testName .. " failed. Expected an error, but no error occurred.")
+  assert(not status, testName .. " failed. Expected error, but got none, expression: " .. expression)
 end
 
 local invalidExpressions = {
@@ -64,6 +77,12 @@ local invalidExpressions = {
   "1 + unknownVariable",
   "1 + 2 + unknownVariable",
   "-unknownVariable",
+  "sin(unknownVariable)",
+  "unknownFunction(1)",
+  "sin(1",
+  "sin(1 + 2",
+  "sin(1,)",
+  "sin(1,"
 }
 
 for _, expression in ipairs(invalidExpressions) do
