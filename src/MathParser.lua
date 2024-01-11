709ee9c2d3d7369f048cca494098bddbafab9732
Luna Davis <ddavi142@asu.edu>
2024-01-11 15:19:44 -0700

    Add support for the new functions to the API, #2

--- src/MathParser.lua
+++ src/MathParser.lua
@@ -1,7 +1,7 @@
 --[[
   Name: MathParser.lua
   Author: ByteXenon [Luna Gilbert]
-  Date: 2024-01-10
+  Date: 2024-01-11
 --]]
 
 -- Localize the path, so this file can be run from anywhere
@@ -41,7 +41,7 @@ end
 -- @param <Table> AST The AST to evaluate.
 -- @return <Number> evaluatedValue The result of the evaluation.
 function MathParserMethods:evaluate(AST)
-  self.Evaluator:resetToInitialState(AST, self.variables, self.operatorFunctions)
+  self.Evaluator:resetToInitialState(AST, self.variables, self.operatorFunctions, self.functions)
   local evaluatedValue = self.Evaluator:evaluate()
   return evaluatedValue
 end
@@ -53,19 +53,35 @@ function MathParserMethods:solve(expression)
   return self:evaluate(self:parse(self:tokenize(expression)))
 end
 
---- Sets a variable to its value.
+--- Adds a variable to its value.
 -- @param <String> variableName The name of the variable.
 -- @param <Number> variableValue The value of the variable.
-function MathParserMethods:setVariable(variableName, variableValue)
+function MathParserMethods:addVariable(variableName, variableValue)
   self.variables = self.variables or {}
   self.variables[variableName] = variableValue
 end
 
---- Sets multiple variables to their values.
+--- Adds multiple variables to their values.
 -- @param <Table> variables The variables to add.
-function MathParserMethods:setVariables(variables)
+function MathParserMethods:addVariables(variables)
   for variableName, variableValue in pairs(variables) do
     self:addVariable(variableName, variableValue)
+  end
+end
+
+--- Adds a function to the evaluator.
+-- @param <String> functionName The name of the function.
+-- @param <Function> functionValue The function to add.
+function MathParserMethods:addFunction(functionName, functionValue)
+  self.functions = self.functions or {}
+  self.functions[functionName] = functionValue
+end
+
+--- Adds multiple functions to the evaluator.
+-- @param <Table> functions The functions to add.
+function MathParserMethods:addFunctions(functions)
+  for functionName, functionValue in pairs(functions) do
+    self:addFunction(functionName, functionValue)
   end
 end
 
@@ -76,17 +92,21 @@ local MathParser = {}
 -- @param <Table> operatorPrecedences The operator precedences to use in the parser.
 -- @param <Table> variables The variables to use in the evaluator.
 -- @param <Table> operatorFunctions The operator functions to evaluate in the evaluator.
+-- @param <Table> functions The functions to use in the evaluator
 -- @return <Table> MathParserInstance The MathParser instance.
-function MathParser:new(operatorPrecedences, variables, operatorFunctions)
+function MathParser:new(operatorPrecedences, variables, operatorFunctions, functions)
   local MathParserInstance = {}
+
   -- Properties
   MathParserInstance.operatorPrecedences = operatorPrecedences
   MathParserInstance.variables = variables
   MathParserInstance.operatorFunctions = operatorFunctions
+  MathParserInstance.functions = functions
+
   -- Classes
   MathParserInstance.Lexer = Lexer:new(nil)
   MathParserInstance.Parser = Parser:new(nil, operatorPrecedences)
-  MathParserInstance.Evaluator = Evaluator:new(nil, variables, operatorFunctions)
+  MathParserInstance.Evaluator = Evaluator:new(nil, variables, operatorFunctions, functions)
 
   local function inheritModule(moduleName, moduleTable)
     for index, value in pairs(moduleTable) do
