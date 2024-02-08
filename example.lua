e6de990b9297d2d59b843bc9c3aee497225206a7
Luna Davis <ddavi142@asu.edu>
2024-02-08 08:16:26 -0700

    Improve the example file

--- example.lua
+++ example.lua
@@ -1,44 +1,54 @@
 -- Dependencies
 local MathParser = require("MathParser")
 
--- Create an instance of MathParser with default settings
+-- Create an instance of MathParser
 local myMathParser = MathParser:new()
 
--- Solve a basic expression
-local result = myMathParser:solve("2 + 2")
-print(result) -- Outputs: 4
+-- Solving a simple expression
+local result = myMathParser:solve("2 + 3")
+print(result)  -- Outputs: 5
 
--- Now, let's add a variable
-myMathParser:addVariable("x", 5)
-result = myMathParser:solve("x + 2")
-print(result) -- Outputs: 7
+-- Adding variables
+myMathParser:addVariable("x", 10)
+myMathParser:addVariable("y", 5)
+result = myMathParser:solve("x + y")
+print(result)  -- Outputs: 15
 
--- Add a function
-myMathParser:addFunction("double", function(a) return a * 2 end)
-result = myMathParser:solve("double(x) * 2")
-print(result) -- Outputs: 20
+-- Adding functions
+myMathParser:addFunction("add", function(a, b)
+  return a + b
+end)
+result = myMathParser:solve("add(x, y)")
+print(result)  -- Outputs: 15
 
--- Now, let's customize operator precedence and functions
-local OPERATOR_PRECEDENCE_LEVELS = {
-  Unary = { ["-"] = 2 },
-  Binary = { ["+"] = 1, ["-"] = 1, ["++"] = 1 }
+-- Custom operators and precedence levels
+local CUSTOM_OPERATOR_PRECEDENCE_LEVELS = {
+  Unary = { ["-"] = 3 },
+  Binary = { ["+"] = 1, ["-"] = 1, ["++"] = 1, ["///"] = 2, ["/"] = 2 }
 }
-
-local OPERATOR_FUNCTIONS = {
+local CUSTOM_OPERATORS = { "+", "-", "++", "///", "/" }
+local CUSTOM_OPERATOR_FUNCTIONS = {
   Unary = { ["-"] = function(a) return -a end },
   Binary = {
     ["+"] = function(a, b) return a + b end,
     ["-"] = function(a, b) return a - b end,
-    ["++"] = function(a, b) return 2 * (a + b) end
+    ["/"] = function(a, b) return a / b end,
+
+    -- Custom operators
+    ["++"] = function(a, b) return 2 * (a + b) end,
+    ["///"] = function(a, b) return 2 * (a / b) end
   }
 }
 
-local VARIABLES, FUNCTIONS = {}, {}
+myMathParser:setOperatorPrecedenceLevels(CUSTOM_OPERATOR_PRECEDENCE_LEVELS)
+myMathParser:setOperators(CUSTOM_OPERATORS)
+myMathParser:setOperatorFunctions(CUSTOM_OPERATOR_FUNCTIONS)
 
--- When you add a custom operator, you have to also add it to the operators table
--- (Unary minus and normal minus signs are the same, so we don't need to add another "-" to the table)
-local OPERATORS = { "+", "-", "++" }
+result = myMathParser:solve("10 ++ 10")
+print(result)  -- Outputs: 40
 
-myMathParser = MathParser:new(OPERATOR_PRECEDENCE_LEVELS, VARIABLES, OPERATOR_FUNCTIONS, OPERATORS, FUNCTIONS)
-result = myMathParser:solve("2 ++ 2")
-print(result) -- Outputs: 8
+result = myMathParser:solve("10 /// 10")
+print(result)  -- Outputs: 2
+
+-- Reset the parser to default settings
+myMathParser:resetToInitialState()
