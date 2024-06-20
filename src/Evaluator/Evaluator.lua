e7f80b49ee26ff06b79f89728114c0fa53abd1f0
Luna Davis <ddavi142@asu.edu>
2024-06-20 01:50:39 -0700

    Refactor the class structure of the Evaluator

--- src/Evaluator/Evaluator.lua
+++ src/Evaluator/Evaluator.lua
@@ -1,7 +1,7 @@
 --[[
   Name: Evaluator.lua
   Author: ByteXenon [Luna Gilbert]
-  Date: 2024-06-14
+  Date: 2024-06-17
 --]]
 
 --* Dependencies *--
@@ -39,135 +39,130 @@ local DEFAULT_FUNCTIONS = {
   deg   = math.deg
 }
 
---* EvaluatorMethods *--
-local EvaluatorMethods = {}
-
---// PRIVATE METHODS \\--
-
---- Evaluates the given unary operator node.
--- @param <Table> node The node to evaluate.
--- @return <Number> result The result of the evaluation.
-function EvaluatorMethods:evaluateUnaryOperator(node)
-  local nodeValue = node.Value
-
-  local operatorFunction = self.operatorFunctions.Unary[nodeValue]
-  assert(operatorFunction, "invalid operator: " .. tostring(nodeValue))
-
-  local operandValue = self:evaluateNode(node.Operand)
-  return operatorFunction(operandValue, node)
-end
-
---- Evaluates the given binary operator node.
--- @param <Table> node The node to evaluate.
--- @return <Number> result The result of the evaluation.
-function EvaluatorMethods:evaluateBinaryOperator(node)
-  local nodeValue = node.Value
-  local nodeLeft = node.Left
-  local nodeRight = node.Right
-
-  local operatorFunction = self.operatorFunctions.Binary[nodeValue]
-  assert(operatorFunction, "invalid operator")
-
-  local leftValue = self:evaluateNode(nodeLeft)
-  local rightValue = self:evaluateNode(nodeRight)
-  return operatorFunction(leftValue, rightValue, node)
-end
-
---- Check what type of operator node it is and evaluates it by calling the appropriate function.
--- @param <Table> node The node to evaluate.
--- @return <Number> result The result of the evaluation.
-function EvaluatorMethods:evaluateOperator(node)
-  local isUnary = not (not node.Operand)
-  if isUnary then
-    return self:evaluateUnaryOperator(node)
-  end
-  return self:evaluateBinaryOperator(node)
-end
-
---- Evaluates a function call node.
--- @param <Table> node The node to evaluate.
--- @return <Number> result The result of the evaluation.
-function EvaluatorMethods:evaluateFunctionCall(node)
-  local functionName = node.FunctionName
-  local arguments = node.Arguments
-
-  local functionCall = self.functions[functionName] or DEFAULT_FUNCTIONS[functionName]
-  assert(functionCall, "invalid function call: " .. tostring(functionName))
-
-  local evaluatedArguments = {}
-  for _, argument in ipairs(arguments) do
-    local evaluatedArgument = self:evaluateNode(argument)
-    insert(evaluatedArguments, evaluatedArgument)
-  end
-
-  return functionCall(unpack(evaluatedArguments))
-end
-
---- Evaluates the given node.
--- @param <Table> node The node to evaluate.
--- @return <Number> result The result of the evaluation.
-function EvaluatorMethods:evaluateNode(node)
-  local nodeType = node.TYPE
-
-  if nodeType == "Constant" then
-    return tonumber(node.Value)
-  elseif nodeType == "Variable" then
-    local variableValue = self.variables[node.Value]
-    if not variableValue then
-      return error("Variable not found: " .. tostring(node.Value))
-    end
-    return variableValue
-  elseif nodeType == "Operator" or nodeType == "UnaryOperator" then
-    return self:evaluateOperator(node)
-  elseif nodeType == "FunctionCall" then
-    return self:evaluateFunctionCall(node)
-  end
-
-  return error("Invalid node type: " .. tostring(nodeType) .. " ( You're not supposed to see this error message. )")
-end
-
---// PUBLIC METHODS \\--
-
---- Resets the evaluator to its initial state.
--- @param <Table> expression The expression to evaluate.
--- @param <Table?> variables={} The variables to use in the evaluator.
--- @param <Table?> operatorFunctions=DEFAULT_OPERATOR_FUNCTIONS The operator functions to evaluate in the evaluator.
-function EvaluatorMethods:resetToInitialState(expression, variables, operatorFunctions, functions)
-  assert(expression, "No expression given")
-
-  self.expression = expression
-  self.variables = variables or {}
-  self.operatorFunctions = operatorFunctions or DEFAULT_OPERATOR_FUNCTIONS
-  self.functions = functions or DEFAULT_FUNCTIONS
-end
-
---- Evaluates the given expression.
--- @return <Number> result The result of the evaluation.
-function EvaluatorMethods:evaluate()
-  assert(self.expression, "No expression to evaluate")
-
-  return self:evaluateNode(self.expression)
-end
-
 --* Evaluator *--
-local Evaluator = {}
-
 --- @class Creates a new Evaluator instance.
 -- @param <Table> expression The expression to evaluate.
 -- @param <Table?> variables={} The variables to use in the evaluator.
 -- @param <Table?> operatorFunctions=DEFAULT_OPERATOR_FUNCTIONS The operator functions to evaluate in the evaluator.
--- @return <Table> EvaluatorInstance The Evaluator instance.
-function Evaluator:new(expression, variables, operatorFunctions, functions)
-  local EvaluatorInstance = {}
-  EvaluatorInstance.expression = expression
-  EvaluatorInstance.variables = variables or {}
-  EvaluatorInstance.operatorFunctions = operatorFunctions or DEFAULT_OPERATOR_FUNCTIONS
-  EvaluatorInstance.functions = functions or {}
+-- @param <Table?> functions=DEFAULT_FUNCTIONS The functions to evaluate in the evaluator.
+-- @return <Table> Evaluator The Evaluator instance.
+local function Evaluator(expression, variables, operatorFunctions, functions)
+  local expression = expression
+  local variables = variables or {}
+  local operatorFunctions = operatorFunctions or DEFAULT_OPERATOR_FUNCTIONS
+  local functions = functions or {}
 
-  -- Main
-  inheritModule("EvaluatorInstance", EvaluatorInstance, "EvaluatorMethods", EvaluatorMethods)
+  local evaluateUnaryOperator, evaluateBinaryOperator,
+        evaluateOperator, evaluateFunctionCall, evaluateNode
 
-  return EvaluatorInstance
+  --- Evaluates the given unary operator node.
+  -- @param <Table> node The node to evaluate.
+  -- @return <Number> result The result of the evaluation.
+  function evaluateUnaryOperator(node)
+    local nodeValue = node.Value
+
+    local operatorFunction = operatorFunctions.Unary[nodeValue]
+    assert(operatorFunction, "invalid operator: " .. tostring(nodeValue))
+
+    local operandValue = evaluateNode(node.Operand)
+    return operatorFunction(operandValue, node)
+  end
+
+  --- Evaluates the given binary operator node.
+  -- @param <Table> node The node to evaluate.
+  -- @return <Number> result The result of the evaluation.
+  function evaluateBinaryOperator(node)
+    local nodeValue = node.Value
+    local nodeLeft = node.Left
+    local nodeRight = node.Right
+
+    local operatorFunction = operatorFunctions.Binary[nodeValue]
+    assert(operatorFunction, "invalid operator")
+
+    local leftValue = evaluateNode(nodeLeft)
+    local rightValue = evaluateNode(nodeRight)
+    return operatorFunction(leftValue, rightValue, node)
+  end
+
+  --- Check what type of operator node it is and evaluates it by calling the appropriate function.
+  -- @param <Table> node The node to evaluate.
+  -- @return <Number> result The result of the evaluation.
+  function evaluateOperator(node)
+    local isUnary = not (not node.Operand)
+    if isUnary then
+      return evaluateUnaryOperator(node)
+    end
+    return evaluateBinaryOperator(node)
+  end
+
+  --- Evaluates a function call node.
+  -- @param <Table> node The node to evaluate.
+  -- @return <Number> result The result of the evaluation.
+  function evaluateFunctionCall(node)
+    local functionName = node.FunctionName
+    local arguments = node.Arguments
+
+    local functionCall = functions[functionName] or DEFAULT_FUNCTIONS[functionName]
+    assert(functionCall, "invalid function call: " .. tostring(functionName))
+
+    local evaluatedArguments = {}
+    for _, argument in ipairs(arguments) do
+      local evaluatedArgument = evaluateNode(argument)
+      insert(evaluatedArguments, evaluatedArgument)
+    end
+
+    return functionCall(unpack(evaluatedArguments))
+  end
+
+  --- Evaluates the given node.
+  -- @param <Table> node The node to evaluate.
+  -- @return <Number> result The result of the evaluation.
+  function evaluateNode(node)
+    local nodeType = node.TYPE
+
+    if nodeType == "Constant" then
+      return tonumber(node.Value)
+    elseif nodeType == "Variable" then
+      local variableValue = variables[node.Value]
+      if not variableValue then
+        return error("Variable not found: " .. tostring(node.Value))
+      end
+      return variableValue
+    elseif nodeType == "Operator" or nodeType == "UnaryOperator" then
+      return evaluateOperator(node)
+    elseif nodeType == "FunctionCall" then
+      return evaluateFunctionCall(node)
+    end
+
+    return error("Invalid node type: " .. tostring(nodeType) .. " ( You're not supposed to see this error message. )")
+  end
+
+  --// PUBLIC METHODS \\--
+
+  --- Resets the evaluator to its initial state.
+  -- @param <Table> expression The expression to evaluate.
+  -- @param <Table?> variables={} The variables to use in the evaluator.
+  -- @param <Table?> operatorFunctions=DEFAULT_OPERATOR_FUNCTIONS The operator functions to evaluate in the evaluator.
+  local function resetToInitialState(expression, variables, operatorFunctions, functions)
+    assert(expression, "No expression given")
+
+    expression = expression
+    variables = variables or {}
+    operatorFunctions = operatorFunctions or DEFAULT_OPERATOR_FUNCTIONS
+    functions = functions or DEFAULT_FUNCTIONS
+  end
+
+  --- Evaluates the given expression.
+  -- @return <Number> result The result of the evaluation.
+  local function evaluate()
+    assert(expression, "No expression to evaluate")
+    return evaluateNode(expression)
+  end
+
+  return {
+    resetToInitialState = resetToInitialState,
+    evaluate = evaluate
+  }
 end
 
 return Evaluator
