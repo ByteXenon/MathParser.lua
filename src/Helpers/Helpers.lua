a92fa02aa3c5a5c2498fe2169d28e5a401e0ac3f
Luna Davis <ddavi142@asu.edu>
2024-06-21 14:17:25 -0700

    Remove used "tableFind" Helpers function

--- src/Helpers/Helpers.lua
+++ src/Helpers/Helpers.lua
@@ -13,19 +13,6 @@ local insert = table.insert
 --* Helpers *--
 local Helpers = {}
 
---- Finds the given value in the given table.
--- @param <Table> table The table to search in.
--- @param <Any> value The value to search for.
--- @return <Any?> index The index of the value in the table.
-function Helpers.tableFind(table, value)
-  for index, tableValue in pairs(table) do
-    if tableValue == value then
-      return index
-    end
-  end
-  return nil
-end
-
 --- Converts the given string to a table of its characters.
 -- @param <String> string The string to convert.
 -- @return <Table> table The table of characters.
